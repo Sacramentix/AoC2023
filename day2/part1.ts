8bc5340b9f091ed02ba8e5ec274f06202fd1e346
Sacramentix <benji.u1225u@gmail.com>
2023-12-02 17:13:02 +0100

    add comment

--- day2/part1.ts
+++ day2/part1.ts
@@ -10,14 +10,14 @@ const colorThreshold:Record<Color, number> = {
 
 const lines = input.split(/\r?\n/);
 
-const result = lines.reduce((acc,line)=>{
-    const [offset, id] = /Game (\d+):/.exec(line)??[] as unknown as [string, string];
+const result = lines.reduce((acc,line)=>{ // we iterate over each line and add ...
+    const [offset, id] = /Game (\d+):/.exec(line)??[] as unknown as [string, string]; // we get the game id with a regex
     return acc + (
-            line.slice(offset.length)
-                .split(";")
-                .map(subset=>[...subset.matchAll(/(\d+) (red|green|blue)/g)].map<[number, Color]>(([_,n,color])=>[Number(n),color as Color]))
-                    .some(subset=>subset.some(([n,color])=> n>colorThreshold[color]))
-            ? 0 : Number(id)
+            line.slice(offset.length) // and we split the game id from it's content using the offset
+                .split(";") // We split each game into subset
+                .map(subset=>[...subset.matchAll(/(\d+) (red|green|blue)/g)].map<[number, Color]>(([_,n,color])=>[Number(n),color as Color])) // we transform each subset into tuple of [number of cube, color of cube]
+                    .some(subset=>subset.some(([n,color])=> n>colorThreshold[color])) // We check if each color of cube is under their respective threshold
+            ? 0 : Number(id) // We add the id of the game only is SOME some of the subset of this game had color above threshold
         );
 },0);
 
--- day2/part2.ts
+++ day2/part2.ts
@@ -11,19 +11,19 @@ const order:Record<Color,number> = {
 
 const lines = input.split(/\r?\n/);
 
-const result = lines.reduce((acc,line)=>{
-    const [offset, id] = /Game (\d+):/.exec(line)??[] as unknown as [string, string];
+const result = lines.reduce((acc,line)=>{ // we iterate over each line and add ...
+    const [offset, id] = /Game (\d+):/.exec(line)??[] as unknown as [string, string]; // we get the game id with a regex
     return acc + 
-            line.slice(offset.length)
-                .split(";")
-                .map(subset=>[...subset.matchAll(/(\d+) (red|green|blue)/g)].map<[number, Color]>(([_,n,color])=>[Number(n),color as Color]))
-                    .reduce((gameAcc, subset)=>
-                        subset.reduce( (subsetAcc,[n,color]) => {
+            line.slice(offset.length) // and we split the game id from it's content using the offset
+                .split(";") // We split each game into subset
+                .map(subset=>[...subset.matchAll(/(\d+) (red|green|blue)/g)].map<[number, Color]>(([_,n,color])=>[Number(n),color as Color])) // we transform each subset into tuple of [number of cube, color of cube]
+                    .reduce((gameAcc, subset)=> // We iterate over each subset of the game and keep track of highest cube quantity per color in a tuple [red cube, green cube,blue cube]
+                        subset.reduce( (subsetAcc,[n,color]) => { // We iterate ovevr each color to check if quantity is higher than previous
                             subsetAcc[order[color]] = n > subsetAcc[order[color]] ? n : subsetAcc[order[color]]
                             return subsetAcc;
                         }, gameAcc)
                     , [0,0,0])
-                    .reduce((acc,colorPower, i) => acc*colorPower,1);
+                    .reduce((acc,colorPower, i) => acc*colorPower,1); // we multiply each color highest quanity together [r,g,b] => 1*r*g*b, that will be added to previous game
 },0);
 
 console.log(result);
