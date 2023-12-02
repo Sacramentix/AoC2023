b3870ce74426bb8fbbff79cce52ae32db1b1daf2
Sacramentix <benji.u1225u@gmail.com>
2023-12-02 17:01:22 +0100

    add comment

--- day1/part1.ts
+++ day1/part1.ts
@@ -3,10 +3,10 @@ const input = Deno.readTextFileSync("./input.txt");
 const lines = input.split(/\r?\n/);
 
 const finalResult = 
-    lines.reduce((result, line) => 
+    lines.reduce((result, line) => // We iterate over all line and add ...
         result + 
-        (Number([...line].find(l=>/\d/.test(l)))*10) +
-        Number([...line].findLast(l=>/\d/.test(l)))
+        (Number([...line].find(l=>/\d/.test(l)))*10) +  // The first digit matched by this regex /\d/ multiplied by 10
+        Number([...line].findLast(l=>/\d/.test(l)))     // And the last digit
     ,0);
 
 console.log(finalResult);
--- day1/part2.ts
+++ day1/part2.ts
@@ -17,20 +17,20 @@ const digits = {
 
 } as const;
 
-const reverse = <T extends string>(s:T):ReverseString<T> => s.split("").reverse().join("") as any;
+const reverse = <T extends string>(s:T):ReverseString<T> => s.split("").reverse().join("") as any; // Take a string and return the reverse. one => eno;
 const reverseKeys = <T extends {}>(o:T):ReverseKeys<T> => Object.entries(o).reduce((acc,[k,v]) => acc[reverse(k)] = v && acc, {} as any);
-const reverseDigits = reverseKeys(digits);
+const reverseDigits = reverseKeys(digits); // Take the digits object and reverse each key and preserve value {one: 1} => {eno: 1}
 
 const matchFirst = <T extends {}>(o:T) => {
     const regex = new RegExp(Object.keys(o).join("|"));
     return (s:string):T[keyof T] => o[regex.exec(s)?.[0] as (keyof typeof o)]
-}
+} // A function that return a function that return the value (1,2,3...) assiocated with the first key (one,two,three...) match.
 
-const matchFirstDigit = matchFirst(digits);
+const matchFirstDigit = matchFirst(digits); // create a function that match digit and return the corresponding number
 const findFirstDigit = (line:string) => matchFirstDigit(line);
 
-const matchFirstReverseDigit = matchFirst(reverseDigits);
-const findLastDigit = (line:string) => matchFirstReverseDigit(reverse(line));;
+const matchFirstReverseDigit = matchFirst(reverseDigits); // create a function that match digit in reverse (eno,owt,eerht...) and return the corresponding number
+const findLastDigit = (line:string) => matchFirstReverseDigit(reverse(line)); // match the first reverse digit while reading line backward
 
 const finalResult = lines.reduce((r, line) => r + (findFirstDigit(line) * 10) + findLastDigit(line) ,0);
 
